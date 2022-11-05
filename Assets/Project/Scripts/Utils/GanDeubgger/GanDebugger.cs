c82c04913f7f311dcb32881939863bc604be715c
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-11-05 16:36:22 +0900

    [011] Add GanDebugger

---
+++ Assets/Project/Scripts/Utils/GanDeubgger/GanDebugger.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GanShin
+{
+    public static class GanDebugger
+    {
+        public static bool IsDebugging = true;
+        
+        public static void Log(string message)
+        {
+            if (!IsDebugging) return;
+            Debug.Log(message);
+        }
+        
+        public static void Log(string header, string message)
+        {
+            if (!IsDebugging) return;
+            Debug.Log($"[{header}]: {message}");
+        }
+        
+        public static void Log(string header, string message, Object context)
+        {
+            if (!IsDebugging) return;
+            Debug.Log($"[{header}]: {message}", context);
+        }
+        
+        public static void LogWarning(string message)
+        {
+            if (!IsDebugging) return;
+            Debug.LogWarning(message);
+        }
+        
+        public static void LogWarning(string header, string message)
+        {
+            if (!IsDebugging) return;
+            Debug.LogWarning($"[{header}]: {message}");
+        }
+        
+        public static void LogWarning(string header, string message, Object context)
+        {
+            if (!IsDebugging) return;
+            Debug.LogWarning($"[{header}]: {message}", context);
+        }
+        
+        public static void LogError(string message)
+        {
+            if (!IsDebugging) return;
+            Debug.LogError(message);
+        }
+        
+        public static void LogError(string header, string message)
+        {
+            if (!IsDebugging) return;
+            Debug.LogError($"[{header}]: {message}");
+        }
+        
+        public static void LogError(string header, string message, Object context)
+        {
+            if (!IsDebugging) return;
+            Debug.LogError($"[{header}]: {message}", context);
+        }
+    }
+}
