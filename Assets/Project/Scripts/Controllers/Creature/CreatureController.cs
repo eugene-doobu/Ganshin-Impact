0a473d7756784c5390d40408887eabdaa4285d0d
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-11-05 15:28:19 +0900

    [006] 공통 로직 CreatureController로 이동

--- Assets/Project/Scripts/Controllers/Creature/CreatureController.cs
+++ Assets/Project/Scripts/Controllers/Creature/CreatureController.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 
 namespace GanShin.Creature
 {
-    public class CreatureController : MonoBehaviour
+    public abstract class CreatureController : MonoBehaviour
     {
+        #region Variables
+
+        private bool     _hasAnimator;
+        private Animator _animator;
+        
+        #endregion Variables
+
+        #region Properties
+
+        protected Animator Animator => _animator;
+        protected bool HasAnimator => _hasAnimator;
+        
+        #endregion Properties
+
+        #region TableData // TODO: 별도의 클래스로 분리 후 json 파일로 관리 예정
+
+        private float _moveSpeed   = 6f;
+        private float _rotateSpeed = 5f;
+
+        #endregion TableData
+        
         #region Mono
         // TODO: MapController의 Init로 변경
         protected virtual void Awake()
         {
-            
+            _hasAnimator = TryGetComponent(out _animator);
         }
 
         protected virtual void Start()
@@ -22,8 +43,10 @@ namespace GanShin.Creature
         // TODO: MapController의 OnUpdate로 변경
         protected virtual void Update()
         {
-        
+            Movement(_moveSpeed);
         }
         #endregion Mono
+
+        protected abstract void Movement(float moveSpeed);
     }
 }
--- Assets/Project/Scripts/Controllers/Creature/PlayerController.cs
+++ Assets/Project/Scripts/Controllers/Creature/PlayerController.cs
@@ -13,22 +13,13 @@ namespace GanShin.Creature
         private CharacterController _characterController;
         
         private Camera   _camera; // TODO: CameraManager 활용 예정
-        private Animator _animator;
-        private bool     _hasAnimator;
 
         private float _jumpTimeoutDelta;
         private float _attackTimeoutDelta;
+
+        private Vector2 _lastMovementValue;
         
         #endregion Variables
-
-        #region TableData // TODO: 별도의 클래스로 분리 후 json 파일로 관리 예정
-
-        private float _moveSpeed = 6f;
-        private float _rotateSpeed = 5f;
-        
-
-        #endregion TableData
-        
         
 
         #region Mono
@@ -38,12 +29,16 @@ namespace GanShin.Creature
             base.Awake();
             
             _input       = Managers.Input;
-            _hasAnimator = TryGetComponent(out _animator);
             _camera      = Camera.main;
             
             _characterController = GetComponent<CharacterController>();
 
             AddInputEvent();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
         }
 
         private void OnDestroy()
@@ -91,8 +86,10 @@ namespace GanShin.Creature
 
         #region Movement
 
-        private void Movement(Vector2 value)
+        protected override void Movement(float moveSpeed)
         {
+            if (_lastMovementValue == Vector2.zero) return;
+            
             // if (_hasAnimator && value == Vector2.zero)
             // {
             //     _animator.SetBool("IsMove", false);
@@ -101,11 +98,13 @@ namespace GanShin.Creature
             //
             // _animator.SetBool("IsMove", true);
 
-            var direction = new Vector3(value.x, 0, value.y);
-            var rotation  = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotateSpeed * Time.deltaTime);
+            var direction = new Vector3(_lastMovementValue.x, 0, _lastMovementValue.y);
+            _characterController.Move(direction * moveSpeed * Time.deltaTime);
+        }
 
-            _characterController.Move(direction * _moveSpeed * Time.deltaTime);
+        private void MovementAnimation()
+        {
+            if (!HasAnimator) return;
         }
 
         #endregion Movement
@@ -117,7 +116,7 @@ namespace GanShin.Creature
         protected virtual void OnMovement(Vector2 value)
         {
             Debug.Log($"[{nameof(Creature)}] OnMovement - value: {value}");
-            Movement(value);
+            _lastMovementValue = value;
         }
         
         protected virtual void OnDash(bool value)
