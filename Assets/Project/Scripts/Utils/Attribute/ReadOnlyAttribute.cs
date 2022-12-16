7fc6c4ea2218dd04b317aa06c0b90de803d2beb6
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-12-17 01:25:30 +0900

    [012] ReadOnly 어트리뷰트 추가

---
+++ Assets/Project/Scripts/Utils/Attribute/ReadOnlyAttribute.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+// reference: https://www.patrykgalach.com/2020/01/20/readonly-attribute-in-unity-editor/
+namespace GanShin.Utils
+{
+    public class ReadOnlyAttribute : PropertyAttribute  {  }
+}
