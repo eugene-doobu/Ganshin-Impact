58a061b3928352fb9bcf06f3bd9ddcd4e8dd7d3d
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-12-24 01:34:17 +0900

    [007] 맵 오브젝트 주석 추가

--- Assets/Project/Scripts/MapObject/MapObject.cs
+++ Assets/Project/Scripts/MapObject/MapObject.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 namespace GanShin.MapObject
 {
+    /// <summary>
+    /// MapController에서 관리하는 오브젝트
+    /// 기본적으로 Collider가 부착되어 있으며,
+    /// 플레이어와의 거리에 따른 이벤트나 마우스 호버 이벤트 등이 존재한다.
+    /// </summary>
     public abstract class MapObject : MonoBehaviour
     {
         // TODO: Apply Readonly
--- Assets/Project/Scripts/MapObject/StaticObject.cs
+++ Assets/Project/Scripts/MapObject/StaticObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace GanShin.MapObject
 {
+    /// <summary>
+    /// 움직이지 않는 오브젝트
+    /// MapObject의 가장 기본적인 구현
+    /// </summary>
     public class StaticObject : MapObject
     {
         public override void OnUpdate()
