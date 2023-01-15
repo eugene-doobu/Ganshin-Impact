5fb3ae8333df1d5ad044eb2b84c70babd69604b0
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-01-15 19:58:53 +0900

    [016] 애니메이션 관련 에셋 정리, 공격 애니메이션 추가

---
+++ Assets/Project/Scripts/Contents/ContentDefine.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GanShin
+{
+    public enum ePlayerAttack
+    {
+        RIKO_BASIC_ATTAK1,
+        RIKO_BASIC_ATTAK2,
+        RIKO_BASIC_ATTAK3,
+        RIKO_BASIC_ATTAK4,
+        RIKO_ULTI_ATTAK1,
+        RIKO_ULTI_ATTAK2,
+        RIKO_ULTI_ATTAK3,
+        RIKO_ULTI_ATTAK4,
+    }
+}
