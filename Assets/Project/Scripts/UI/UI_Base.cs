272ef97cc479ae66a6bfbd6fb033aaa9156911f2
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-01-20 23:29:57 +0900

    [029] 레거시 코드 제거

--- Assets/Project/Scripts/UI/UI_Base.cs
+++ Assets/Project/Scripts/UI/UI_Base.cs
@@ -1,67 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using GanShin;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public abstract class UI_Base : MonoBehaviour
+namespace GanShin.UI
 {
-	protected Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
-	public abstract void Init();
-
-	private void Start()
-	{
-		Init();
-	}
-
-	protected void Bind<T>(Type type) where T : UnityEngine.Object
-	{
-		string[] names = Enum.GetNames(type);
-		UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-		_objects.Add(typeof(T), objects);
-
-		for (int i = 0; i < names.Length; i++)
-		{
-			if (typeof(T) == typeof(GameObject))
-				objects[i] = Util.FindChild(gameObject, names[i], true);
-			else
-				objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-
-			if (objects[i] == null)
-				GanDebugger.Log($"Failed to bind({names[i]})");
-		}
-	}
-
-	protected T Get<T>(int idx) where T : UnityEngine.Object
-	{
-		UnityEngine.Object[] objects = null;
-		if (_objects.TryGetValue(typeof(T), out objects) == false)
-			return null;
-
-		return objects[idx] as T;
-	}
-
-	protected GameObject GetObject(int idx) { return Get<GameObject>(idx); }
-	protected Text GetText(int idx) { return Get<Text>(idx); }
-	protected Button GetButton(int idx) { return Get<Button>(idx); }
-	protected Image GetImage(int idx) { return Get<Image>(idx); }
-
-	public static void BindEvent(GameObject go, Action<PointerEventData> action, Define.eUIEvent type = Define.eUIEvent.Click)
-	{
-		UI_EventHandler evt = Util.GetOrAddComponent<UI_EventHandler>(go);
-
-		switch (type)
-		{
-			case Define.eUIEvent.Click:
-				evt.OnClickHandler -= action;
-				evt.OnClickHandler += action;
-				break;
-			case Define.eUIEvent.Drag:
-				evt.OnDragHandler -= action;
-				evt.OnDragHandler += action;
-				break;
-		}
-	}
+    public abstract class UI_Base : MonoBehaviour
+    {
+    }
 }
