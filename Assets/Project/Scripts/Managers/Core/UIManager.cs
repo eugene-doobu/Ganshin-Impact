19c23553a2ef6b028c5474d6639a148e7caace6f
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-01-21 00:14:29 +0900

    [029] UIManager에서 Context를 관리하는 기능 추가

--- Assets/Project/Scripts/Managers/Core/UIManager.cs
+++ Assets/Project/Scripts/Managers/Core/UIManager.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using GanShin.AssetManagement;
 using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace GanShin.UI
@@ -11,6 +12,8 @@ namespace GanShin.UI
     public partial class UIManager : IInitializable
     {
 	    [Inject] private ResourceManager _resource;
+	    
+	    private readonly Dictionary<Type ,INotifyPropertyChanged> _dataContexts = new();
 
 	    public GameObject GlobalRoot { get; private set; } = null;
 
@@ -18,6 +21,36 @@ namespace GanShin.UI
 	    public UIManager()
 	    {
 	    }
+
+	    #region Context Management Interface
+	    public T GetContext<T>() where T : class, INotifyPropertyChanged
+	    {
+		    if(_dataContexts.ContainsKey(typeof(T)))
+			    return _dataContexts[typeof(T)] as T;
+		    return null;
+	    }
+	    
+	    public T GetOrAddContext<T>() where T : class, INotifyPropertyChanged, new()
+	    {
+		    if(_dataContexts.ContainsKey(typeof(T)))
+			    return _dataContexts[typeof(T)] as T;
+		    var context = new T();
+		    AddContext(context);
+		    return context;
+	    }
+	    
+	    public void AddContext<T>(T context) where T : class, INotifyPropertyChanged
+	    {
+		    var result = _dataContexts.TryAdd(typeof(T), context);
+		    if (!result)
+			    GanDebugger.LogWarning(nameof(UIManager), "Context already exists");
+		}
+	    
+	    public void RemoveContext<T>(T context) where T : class, INotifyPropertyChanged
+	    {
+		    _dataContexts.Remove(typeof(T));
+	    }
+	    #endregion Context Management Interface
 
 	    public void Initialize()
 	    {
