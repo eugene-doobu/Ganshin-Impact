50d9920b25dbc9edbf0f1eacbdc646044d021a67
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-02-10 21:59:10 +0900

    [032] nav mesh agent 기반 이동 구현

--- Assets/Project/Scripts/Contents/Creature/Monster/FieldMonster/FieldMonsterController.cs
+++ Assets/Project/Scripts/Contents/Creature/Monster/FieldMonster/FieldMonsterController.cs
@@ -1,15 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
+#nullable enable
+
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GanShin.Content.Creature.Monster
 {
+    [RequireComponent(typeof(Animator), typeof(NavMeshAgent),typeof(CapsuleCollider))]
     public class FieldMonsterController : MonsterController
     {
+        [SerializeField] private float _monsterSight        = 10f;
         [SerializeField] private float rotationSmoothFactor = 8f;
 
+        private Animator        _animator        = null!;
+        private NavMeshAgent    _navMeshAgent    = null!;
+        private CapsuleCollider _capsuleCollider = null!;
+
+#region MonoBehaviour
+        protected override void Awake()
+        {
+            base.Awake();
+            _animator        = GetComponent<Animator>();
+            _navMeshAgent    = GetComponent<NavMeshAgent>();
+            _capsuleCollider = GetComponent<CapsuleCollider>();
+        }
+        
         protected override void Start()
         {
             base.Start();
@@ -21,16 +35,38 @@ namespace GanShin.Content.Creature.Monster
             Target = other.transform;
             State  = eMonsterState.TRACING;
         }
+#endregion MonoBehaviour
 
-        // TODO: A star 알고리즘을 이용한 이동
+#region Initalize
+        private void Initialize()
+        {
+            InitializeNavMeshAgent();
+            InitializeCapsuleCollider();
+        }
+
+        private void InitializeNavMeshAgent()
+        {
+            _navMeshAgent.speed = _moveSpeed;
+        }
+
+        private void InitializeCapsuleCollider()
+        {
+            _capsuleCollider.center    = new Vector3(0, 0.5f, 0);
+            _capsuleCollider.isTrigger = true;
+            _capsuleCollider.radius    = _monsterSight;
+        }
+#endregion Initalize
+        
+
         protected override void Movement(float moveSpeed)
         {
         }
 
 #region ProcessState
-
         protected override void ProcessCreated()
         {
+            Initialize();
+            State = eMonsterState.IDLE;
         }
 
         protected override void ProcessIdle()
@@ -40,8 +76,11 @@ namespace GanShin.Content.Creature.Monster
         protected override void ProcessTracing()
         {
             if (ReferenceEquals(Target, null)) return;
+            
+            var targetPosition = Target.position;
+            _navMeshAgent.destination = targetPosition;
 
-            var direction = (Target.position - transform.position).normalized;
+            var direction = (targetPosition - transform.position).normalized;
             direction = Vector3.ProjectOnPlane(direction, Vector3.up);
 
             var targetRotation = Quaternion.LookRotation(direction);
@@ -60,7 +99,6 @@ namespace GanShin.Content.Creature.Monster
         protected override void ProcessDead()
         {
         }
-
 #endregion ProcessState
     }
 }
