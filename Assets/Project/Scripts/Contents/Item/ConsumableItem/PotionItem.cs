3fd94ab071f78b80923aebf880a952733dec4548
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-09-02 21:13:12 +0900

    [066] 아이템 자식 클래스 구현

--- Assets/Project/Scripts/Contents/Item/ConsumableItem/PotionItem.cs
+++ Assets/Project/Scripts/Contents/Item/ConsumableItem/PotionItem.cs
@@ -1,7 +1,16 @@
 namespace GanShin.Space.Content
 {
-    public class PotionItem
+    public class PotionItem : ConsumableItem
     {
+        public override void Use()
+        {
+            // TODO
+        }
         
+        public override void Initialize(InventoryManager owner)
+        {
+            base.Initialize(owner);
+            Type = ConsumableItemType.POTION;
+        }
     }
 }
--- Assets/Project/Scripts/Contents/Item/ConsumableItem/StaminaPotionItem.cs
+++ Assets/Project/Scripts/Contents/Item/ConsumableItem/StaminaPotionItem.cs
@@ -1,7 +1,22 @@
 namespace GanShin.Space.Content
 {
-    public class StaminaPotionItem
+    public class StaminaPotionItem : ConsumableItem
     {
+        private float _healAmount = 25;
         
+        public override void Use()
+        {
+            var playerManager = Owner.PlayerManager;
+            if (playerManager == null)
+                return;
+            
+            playerManager.CurrentStamina += _healAmount;
+        }
+        
+        public override void Initialize(InventoryManager owner)
+        {
+            base.Initialize(owner);
+            Type = ConsumableItemType.STAMINA_POTION;
+        }
     }
 }
