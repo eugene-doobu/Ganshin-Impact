1ecd4bce58203254b41d07063f7eea2e7e91b025
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-09-10 20:09:46 +0900

    [063] 새로 바뀐 대화 구조에 맞게 디버그용 함수 수정

--- Assets/Project/Scripts/Scenes/SimpleDemoScene.cs
+++ Assets/Project/Scripts/Scenes/SimpleDemoScene.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using GanShin.Dialogue.Base;
 using GanShin.Space.Content;
 using GanShin.UI;
 using UnityEngine;
@@ -108,13 +109,37 @@ namespace GanShin.SceneManagement
                 _dialogueManager.StartDialogue();
             
             if (Input.GetKeyDown("2"))
-                _dialogueManager.SetString("hello");
-            
+            {
+                var dialogueInfo = new DialogueInfo
+                {
+                    name = "나는 누굴까",
+                    content = "hello",
+                    npcDialogueImage = ENpcDialogueImage.NONE,
+                };
+                _dialogueManager.SetString(dialogueInfo);
+            }
+
             if (Input.GetKeyDown("3"))
-                _dialogueManager.SetString("밥을 어디 옷을 천하를 불어 품으며, 소담스러운 쓸쓸하랴? 사라지지 하는 인생에 이것이다. 더운지라 열락의 실현에 안고, 그들은 우리 불러 그와 가슴이 것이다. 위하여서 인생의 물방아 끓는다. 생명을 수 소금이라 거친 몸이 그림자는 타오르고 놀이 봄바람이다. 품으며, 구하기 내는 인생에 없는 때에, 실로 기쁘며, 인간은 사막이다.");
+            {
+                var dialogueInfo = new DialogueInfo
+                {
+                    name             = "Riko",
+                    content          = "밥을 어디 옷을 천하를 불어 품으며, 소담스러운 쓸쓸하랴? 사라지지 하는 인생에 이것이다. 더운지라 열락의 실현에 안고, 그들은 우리 불러 그와 가슴이 것이다. 위하여서 인생의 물방아 끓는다. 생명을 수 소금이라 거친 몸이 그림자는 타오르고 놀이 봄바람이다. 품으며, 구하기 내는 인생에 없는 때에, 실로 기쁘며, 인간은 사막이다.",
+                    npcDialogueImage = ENpcDialogueImage.RIKO,
+                };
+                _dialogueManager.SetString(dialogueInfo);
+            }
             
             if (Input.GetKeyDown("4"))
-                _dialogueManager.SetString("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,");
+            {
+                var dialogueInfo = new DialogueInfo
+                {
+                    name             = "나는 누굴까",
+                    content          = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
+                    npcDialogueImage = ENpcDialogueImage.NONE,
+                };
+                _dialogueManager.SetString(dialogueInfo);
+            }
         }
 
         public override void Clear()
