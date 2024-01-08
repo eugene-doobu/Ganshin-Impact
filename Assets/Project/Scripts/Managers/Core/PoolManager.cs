6be220fa3b3f5120738df30817885aeb3683ef52
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-01-08 23:54:01 +0900

    [080] PoolManager 추가

---
+++ Assets/Project/Scripts/Managers/Core/PoolManager.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Pool
+{
+    private GameObject              _prefab;
+    private IObjectPool<GameObject> _pool;
+
+    private Transform _root;
+    private Transform Root
+    {
+        get
+        {
+            if (_root != null) return _root;
+            
+            var go = new GameObject() { name = $"@{_prefab.name}Pool" };
+            _root = go.transform;
+
+            return _root;
+        }
+    }
+
+    public Pool(GameObject prefab)
+    {
+        _prefab = prefab;
+        _pool   = new ObjectPool<GameObject>(OnCreate, OnGet, OnRelease, OnDestroy);
+    }
+
+    public void Push(GameObject go)
+    {
+        if(go.activeSelf)
+            _pool.Release(go);
+    }
+
+    public GameObject Pop()
+    {
+        return _pool.Get();
+    }
+
+#region Funcs
+    GameObject OnCreate()
+    {
+        GameObject go = Object.Instantiate(_prefab, Root, true);
+        go.name = _prefab.name;
+        return go;
+    }
+
+    void OnGet(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+    void OnRelease(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    void OnDestroy(GameObject go)
+    {
+        Object.Destroy(go);
+    }
+#endregion
+}
+
+namespace GanShin.Resource
+{
+    public class PoolManager : MonoBehaviour
+    {
+        private Dictionary<string, Pool> _pools = new();
+
+        public GameObject Pop(GameObject prefab)
+        {
+            if (_pools.ContainsKey(prefab.name) == false)
+                CreatePool(prefab);
+
+            return _pools[prefab.name].Pop();
+        }
+
+        public bool Push(GameObject go)
+        {
+            if (_pools.ContainsKey(go.name) == false)
+                return false;
+
+            _pools[go.name].Push(go);
+            return true;
+        }
+
+        public void Clear()
+        {
+            _pools.Clear();
+        }
+
+        void CreatePool(GameObject original)
+        {
+            var pool = new Pool(original);
+            _pools.Add(original.name, pool);
+        }
+    }
+}
