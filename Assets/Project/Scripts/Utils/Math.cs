e0fce237d3c811850eb39b1761c94fa53ee1f038
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-02-22 22:34:18 +0900

    add quaternion math utils

--- Assets/Project/Scripts/Utils/Math.cs
+++ Assets/Project/Scripts/Utils/Math.cs
@@ -14,8 +14,8 @@ namespace GanShin
         public static float GetRandomValue(float value)
         {
             return Random.Range(-value, value);
-        }    
-        
+        }
+
         public static float GetRandomUnsignedValue(float value)
         {
             return Random.Range(0, value);
@@ -37,6 +37,35 @@ namespace GanShin
             result.y = GetRandomUnsignedValue(value.y);
             result.z = GetRandomUnsignedValue(value.z);
             return result;
+        }
+
+        public static void Decompose(this Quaternion quaternion, Vector3 direction, out Quaternion swing, out Quaternion twist)
+        {
+            var vector     = new Vector3(quaternion.x, quaternion.y, quaternion.z);
+            var projection = Vector3.Project(vector, direction);
+
+            twist = new Quaternion(projection.x, projection.y, projection.z, quaternion.w);
+            twist.Normalize();
+            swing = quaternion * Quaternion.Inverse(twist);
+        }
+
+        public static Quaternion Constrain(this Quaternion quaternion, float angle)
+        {
+            var magnitude    = Mathf.Sin(0.5f * angle);
+            var sqrMagnitude = magnitude * magnitude;
+
+            var vector = new Vector3(quaternion.x, quaternion.y, quaternion.z);
+
+            if (!(vector.sqrMagnitude > sqrMagnitude)) return quaternion;
+
+            vector = vector.normalized * magnitude;
+
+            quaternion.x = vector.x;
+            quaternion.y = vector.y;
+            quaternion.z = vector.z;
+            quaternion.w = Mathf.Sqrt(1.0f - sqrMagnitude) * Mathf.Sign(quaternion.w);
+
+            return quaternion;
         }
     }
 }
